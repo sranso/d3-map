683310138af4c915a79dce8a7e8fffbe7c6ff508
sranso <sranso@gmail.com>
2014-02-10 10:53:09 -0500

    fix map, now it twerks

--- map2.js
+++ map2.js
@@ -1,6 +1,8 @@
 var h = 960,
     w = 960;
 
+// an azimuthal projection suitable for displaying a single hemisphere
+// point of perspective is at infinity
 var projection = d3.geo.orthographic()
                        .scale(350)
                        .translate([w/2, h/2]) // place globe in center of svg
@@ -17,10 +19,13 @@ var svg = d3.select("body").append("svg")
     .attr("width", w)
     .attr("height", h);
 
+// this makes an outer sphere. i can color this with blue b/c of the id
+// defs: defining a path before i put everything down
 svg.append("defs").append("path")
    .datum({type: "Sphere"}) // built into d3, draw a sphere on the svg
    .attr("id", "sphere")
-   .attr("d", path); // d stores path coordinates, draws path on svg
+   // d is attribute of a path that holds its coordinates, draws path on svg
+   .attr("d", path);
 
 svg.append("path")
    .datum(graticule)
@@ -28,14 +33,22 @@ svg.append("path")
    .attr("d", path);
 
 d3.json("map.json", function(error, world){
+// inserting boundary lines b/t countries
+    svg.insert("path", ".graticule")
+       .datum(topojson.feature(world, world.objects.land))
+       .attr("class", "land")
+       .attr("d", path);
+
+    // puts down mesh b/t the countries
     svg.insert("path", ".graticule")
        .datum(topojson.mesh(world, world.objects.countries, function(a, b) {
           return a !== b;
+          // make a line where a country is different
        }))
        .attr("class", "boundary")
        .attr("d", path);
 
-    svg.call("drag");
+    svg.call(drag);
 
 });
 
@@ -65,6 +78,4 @@ var drag = d3.behavior.drag().on('drag', function() {
 
   svg.selectAll("path").attr("d", path);
 })
-
-
 
