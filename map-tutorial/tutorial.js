7baea6baf943b01f48411255446a97d99040258a
sranso <sranso@gmail.com>
2014-02-15 13:54:41 -0500

    rearrange files, get it working at least a little

--- map-tutorial/tutorial.js
+++ map-tutorial/tutorial.js
@@ -1,14 +1,23 @@
 var width = 960,
     height = 1160;
 
+// extract the definition of the projection
+var projection = d3.geo.mercator()
+    .scale(500)
+    .translate([width / 2, height / 2]);
+
+// path generator
+var path = d3.geo.path()
+    .projection(projection);
+
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
 d3.json("uk.json", function(error, uk) {
-  console.log(uk);
+  // path element
   svg.append("path")
-     .datum(topojson.feature(uk, uk.objects.subunits))
-     .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+      // have to convert back to geoJSON for display. below () does that.
+      .datum(topojson.feature(uk, uk.objects.subunits))
+      .attr("d", path);
 });
-
