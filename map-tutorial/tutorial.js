02322b668c8fea5583498729475a17297f1e0054
sranso <sranso@gmail.com>
2014-02-15 17:23:36 -0500

    HOVER GALORE BIATCHES

--- map-tutorial/tutorial.js
+++ map-tutorial/tutorial.js
@@ -1,25 +1,19 @@
 var width = 960,
     height = 1160;
 
-// extract the definition of the projection
-var projection = d3.geo.albers()
-    .center([0, 55.4]) // set center
-    .rotate([4.4, 0]) // rotate lat and lon
-    .parallels([50, 60])
-    .scale(1200 * 5)
-    .translate([width / 2, height / 2]);
-
-// path generator
-var path = d3.geo.path()
-    .projection(projection)
-    .pointRadius(2);
-
 var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+                           .attr("width", width)
+                           .attr("height", height);
 
 d3.json("uk.json", function(error, uk) {
   var subunits = topojson.feature(uk, uk.objects.subunits),
+      projection = d3.geo.albers().center([0, 55.4]) // set center
+                     .rotate([4.4, 0]) // rotate lat and lon
+                     .parallels([50, 60])
+                     .scale(6000)
+                     .translate([width / 2, height / 2]),
+      // path generator
+      path = d3.geo.path().projection(projection).pointRadius(4),
       places = topojson.feature(uk, uk.objects.places);
 
   svg.selectAll(".subunit")
@@ -27,7 +21,13 @@ d3.json("uk.json", function(error, uk) {
       .data(subunits.features)
     .enter().append("path")
       .attr("class", function(d) { return "subunit " + d.id; })
-      .attr("d", path);
+      .attr("d", path)
+      .on("mouseover", function() {
+        d3.select(this).attr("opacity", "0.5");  
+      })
+      .on("mouseout", function() {
+        d3.select(this).attr("opacity", "1");
+      });
 
   // make borders
   svg.append("path")
@@ -41,39 +41,30 @@ d3.json("uk.json", function(error, uk) {
       .attr("class", "subunit-boundary IRL");
 
   // add countries' names
-  svg.selectAll(".subunit-label")
-      .data(subunits.features)
-    .enter().append("text")
-      .attr("class", function(d) { return "subunit-label " + d.id; })
-      .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .text(function(d) { return d.properties.name; });
+  // svg.selectAll(".subunit-label")
+  //     .data(subunits.features)
+  //   .enter().append("text")
+  //     .attr("class", function(d) { return "subunit-label " + d.id; })
+  //     .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
+  //     .attr("dy", ".35em")
+  //     .text(function(d) { return d.properties.name; });
 
   // add small circle for each city
-  svg.append("path")
-      .datum(places)
+  svg.selectAll(".place")
+      .data(places.features)
+      .enter().append("path")
       .attr("d", path)
-      .attr("class", "place")
       .on("mouseover", function(d) {
         svg.append("text")
-          .attr("class", "place-label")
-          .attr("transform", "translate(10)")
-          .attr("x", 10);
+            .attr("class", "place-label")
+            .attr("transform", function() { return "translate(" + projection(d.geometry.coordinates) + ")"; })
+            .attr("x", function() { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
+            .attr("dy", ".35em")
+            .style("text-anchor", function() { return d.geometry.coordinates[0] > -1 ? "start" : "end"; })
+            .text(function() { return d.properties.name; });
+      })
+      .on("mouseout", function(d) {
+        d3.select("text").remove();
       });
 
-  // add city names
-  // change position of city names
-  svg.selectAll(".place-label")
-      .data(places.features)
-    .enter().append("text")
-      .attr("class", "place-label")
-      .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
-      .attr("x", function(d) { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
-      .attr("dy", ".35em")
-      .style("text-anchor", function(d) { return d.geometry.coordinates[0] > -1 ? "start" : "end"; })
-      .text(function(d) { return d.properties.name; });
 });
-
-function mouseover(d, i) {
-  d3.select(rect)
-}
