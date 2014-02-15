4bddfef8686289ab2ec6e3a48cc0bb741ee8b1df
sranso <sranso@gmail.com>
2014-02-15 14:42:05 -0500

    finish tutorial

--- map-tutorial/tutorial.js
+++ map-tutorial/tutorial.js
@@ -11,31 +11,58 @@ var projection = d3.geo.albers()
 
 // path generator
 var path = d3.geo.path()
-    .projection(projection);
+    .projection(projection)
+    .pointRadius(2);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
 d3.json("uk.json", function(error, uk) {
-  // style polygons, add borders
+  var subunits = topojson.feature(uk, uk.objects.subunits),
+      places = topojson.feature(uk, uk.objects.places);
+
   svg.selectAll(".subunit")
-      .data(topojson.feature(uk, uk.objects.subunits).features)
-    .enter().append("path") // data join
+  // style polygons, add borders
+      .data(subunits.features)
+    .enter().append("path")
       .attr("class", function(d) { return "subunit " + d.id; })
       .attr("d", path);
 
   svg.append("path")
-      .datum(topojson.mesh(uk, uk.objects.subunits, function(a, b) {
-        return a !== b && a.id !== "IRL"; 
-      }))
+      .datum(topojson.mesh(uk, uk.objects.subunits, function(a, b) { return a !== b && a.id !== "IRL"; }))
       .attr("d", path)
       .attr("class", "subunit-boundary");
 
   svg.append("path")
-      .datum(topojson.mesh(uk, uk.objects.subunits, function(a, b) {
-        return a === b && a.id === "IRL"; 
-      }))
+      .datum(topojson.mesh(uk, uk.objects.subunits, function(a, b) { return a === b && a.id === "IRL"; }))
       .attr("d", path)
       .attr("class", "subunit-boundary IRL");
+
+  // add countries' names
+  svg.selectAll(".subunit-label")
+      .data(subunits.features)
+    .enter().append("text")
+      .attr("class", function(d) { return "subunit-label " + d.id; })
+      .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.properties.name; });
+
+  // add small circle for each city
+  svg.append("path")
+      .datum(places)
+      .attr("d", path)
+      .attr("class", "place");
+
+  // add city names
+  // change position of city names
+  svg.selectAll(".place-label")
+      .data(places.features)
+    .enter().append("text")
+      .attr("class", "place-label")
+      .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
+      .attr("x", function(d) { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
+      .attr("dy", ".35em")
+      .style("text-anchor", function(d) { return d.geometry.coordinates[0] > -1 ? "start" : "end"; })
+      .text(function(d) { return d.properties.name; });
 });
