b1a654e85d94eec0a141176530c43b3f165011a8
sranso <sranso@gmail.com>
2014-03-03 15:30:51 -0500

    play with new charts for bffgroove

--- new-map/newmap.js
+++ new-map/newmap.js
@@ -5,6 +5,26 @@ var svg = d3.select(".container").append("svg")
                            .attr("width", width)
                            .attr("height", height);
 
+var drag = d3.behavior.drag()
+    .origin(Object)
+    .on("dragstart", dragstarted)
+    .on("drag", dragged)
+    .on("dragend", dragended);
+
+function dragstarted(d) {
+  d3.event.sourceEvent.stopPropagation();
+  d3.select(this).classed("dragging", true);
+  console.log(d);
+}
+
+function dragged(d) {
+  d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+}
+
+function dragended(d) {
+  d3.select(this).classed("dragging", false);
+}
+
 d3.json("countries.json", function(error, countries) {
   var projection = d3.geo.orthographic().clipAngle(90)
                                   .translate([width / 2, height / 2]),
@@ -15,7 +35,8 @@ d3.json("countries.json", function(error, countries) {
     .enter()
     .append("path")
     .attr("d", path)
-    .attr("class", "country");
+    .attr("class", "country")
+    .call(drag);
 
     // albers has to be calibrated 
 
