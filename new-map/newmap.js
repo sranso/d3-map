ad7af2df7af519378a9a16a9da1bb564f4bff515
sranso <sranso@gmail.com>
2014-03-03 22:37:39 -0500

    drag works

--- new-map/newmap.js
+++ new-map/newmap.js
@@ -5,38 +5,21 @@ var svg = d3.select(".container").append("svg")
                            .attr("width", width)
                            .attr("height", height);
 
-var drag = d3.behavior.drag()
-    .origin(Object)
-    .on("dragstart", dragstarted)
-    .on("drag", dragged)
-    .on("dragend", dragended);
-
-function dragstarted(d) {
-  d3.event.sourceEvent.stopPropagation();
-  d3.select(this).classed("dragging", true);
-  console.log(d);
-}
-
-function dragged(d) {
-  d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
-}
-
-function dragended(d) {
-  d3.select(this).classed("dragging", false);
-}
+var projection;
 
 d3.json("countries.json", function(error, countries) {
-  var projection = d3.geo.orthographic().clipAngle(90)
-                                  .translate([width / 2, height / 2]),
-      path = d3.geo.path().projection(projection);
+  projection = d3.geo.orthographic().clipAngle(90).translate([width / 2, height / 2]),
+  path = d3.geo.path().projection(projection);
 
   svg.selectAll(".country")
-    .data(topojson.feature(countries, countries.objects.subunits).features)
+      .data(topojson.feature(countries, countries.objects.subunits).features)
     .enter()
-    .append("path")
-    .attr("d", path)
-    .attr("class", "country")
-    .call(drag);
+      .append("path")
+      .attr("d", path)
+      .attr("class", "country")
+      .attr("fill", "rgb(79,151,27");
+
+  svg.call(drag);
 
     // albers has to be calibrated 
 
@@ -44,4 +27,31 @@ d3.json("countries.json", function(error, countries) {
   //    .datum(topojson.feature(countries, countries.objects.subunits))
   //    .attr("d", path);
 
+});
+
+var drag = d3.behavior.drag().on('drag', function() {
+  var start = { 
+    lon: projection.rotate()[0], 
+    lat: projection.rotate()[1]
+  },
+
+  delta = { 
+    x: d3.event.dx,
+    y: d3.event.dy  
+  },
+    
+  scale = 0.25,
+
+  end = { 
+    lon: start.lon + delta.x * scale, 
+    lat: start.lat - delta.y * scale 
+  };
+
+  end.lat = end.lat >  30 ?  30 :
+            end.lat < -30 ? -30 :
+            end.lat;
+  
+  projection.rotate([end.lon,end.lat]);
+
+  svg.selectAll("path").attr("d", path);
 });
